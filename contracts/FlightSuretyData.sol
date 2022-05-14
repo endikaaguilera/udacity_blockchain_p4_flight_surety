d930891caa5c529688331effe14fc1b881156436
EndikaAguilera <matiax28@gmail.com>
2022-05-14 22:45:59 +0200

    FlightSuretyData

--- contracts/FlightSuretyData.sol
+++ contracts/FlightSuretyData.sol
@@ -14,16 +14,69 @@ contract FlightSuretyData {
     address private contractOwner; // Account used to deploy contract
     bool private operational = true; // Blocks all state changes throughout the contract if false
 
+    struct Insured {
+        address addr;
+        uint256 insuredAmount;
+        bool isPayed;
+    }
+
+    struct Airline {
+        address addr;
+        string name;
+        uint256 etherAmount;
+        bool isRegistered;
+        bool isFunded;
+    }
+
+    address[] private airlines;
+    uint256 private totalApprovedAirlines = 0;
+
+    //list of authorized callers
+    address[] private authorizedCallers;
+
+    //mapping for all the operating airlines
+    mapping(address => Airline) private operatingAirlines;
+
+    //Not used now
+    mapping(address => uint256) balanceOf;
+
+    //mapping to store all flight insurees
+    mapping(bytes32 => Insured[]) private flightInsurees;
+
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
     /********************************************************************************************/
+
+    //fired when an Airline registers
+    event RegisterAirline(address airline, string name);
+
+    //fires when a passenger buys insurance
+    event BuyFlightInsurance(bytes32 flight, address insuree);
+
+    //Used when insuree withdraws funds
+    event InsurancePayout(bytes32 flight, address insuree);
+
+    //used when airline is funded
+    event FundAirline(address airline);
 
     /**
      * @dev Constructor
      *      The deploying account becomes contractOwner
      */
-    constructor() {
+    constructor(address firstAirline) {
         contractOwner = msg.sender;
+
+        // Initialize first airline
+        Airline memory newAirline = Airline(
+            firstAirline,
+            "EAA AIRLINES",
+            0,
+            true,
+            false
+        );
+        airlines.push(firstAirline);
+        operatingAirlines[firstAirline] = newAirline;
+        totalApprovedAirlines += 1;
     }
 
     /********************************************************************************************/
@@ -82,31 +135,251 @@ contract FlightSuretyData {
      *      Can only be called from FlightSuretyApp contract
      *
      */
-    function registerAirline() external pure {}
+    function registerAirline(
+        address addr,
+        string memory name,
+        bool register
+    ) external requireIsOperational {
+        //Hash the components to store for mapping
+        //bytes32 hashedAirline = keccak256(abi.encodePacked(index, addr, name));
+
+        Airline memory newAirline = Airline(addr, name, 0, register, false);
+        airlines.push(addr);
+        if (register) {
+            totalApprovedAirlines++;
+        }
+
+        //uint256 balance = balanceOf[address];
+        operatingAirlines[addr] = newAirline;
+
+        emit RegisterAirline(addr, name);
+    }
 
     /**
-     * @dev Buy insurance for a flight
+     * @dev Check if Airline exists
+     *
      *
      */
-    function buy() external payable {}
+    function isAirline(address caller) external view returns (bool) {
+        Airline memory existingAirline = operatingAirlines[caller];
+        return existingAirline.addr != address(0);
+    }
+
+    /**
+     * @dev Check if Airline is funded
+     *
+     *
+     */
+    function isAirlineFunded(address airline) external view returns (bool) {
+        Airline storage fundedAirline = operatingAirlines[airline];
+        return fundedAirline.isFunded;
+    }
+
+    /**
+     * @dev Check if Airline is registered
+     *
+     */
+    function isAirlineRegistered(address airline)
+        external
+        view
+        requireIsOperational
+        returns (bool)
+    {
+        Airline storage fundedAirline = operatingAirlines[airline];
+        return fundedAirline.isRegistered;
+    }
+
+    /**
+     * @dev Get appoved Airlines count
+     *
+     */
+    function getAirlinesCount() external view returns (uint256) {
+        return totalApprovedAirlines;
+    }
+
+    /**
+     * @dev Get Airline by index
+     *
+     */
+    function getAirlineByIndex(uint256 index) external view returns (address) {
+        return airlines[index];
+    }
+
+    /**
+     * @dev Vote for an airline and update registration
+     *      only can be called from FlightSuretyApp contract
+     *
+     */
+    function updateAirlineIsRegistered(address addr, bool register)
+        external
+        requireIsOperational
+    {
+        Airline memory updateAirline = operatingAirlines[addr];
+        updateAirline.isRegistered = register;
+
+        operatingAirlines[addr] = updateAirline;
+        if (register) {
+            totalApprovedAirlines += 1; //.add(1);
+        }
+    }
 
     /**
      *  @dev Credits payouts to insurees
      */
-    function creditInsurees() external pure {}
+    function creditInsureeAmount(bytes32 flight, address passenger)
+        external
+        view
+        requireIsOperational
+        returns (uint256)
+    {
+        Insured[] memory insurees = flightInsurees[flight];
+        uint256 payout = 0;
+        for (uint256 i = 0; i < insurees.length; i++) {
+            if (insurees[i].addr == passenger) {
+                payout = insurees[i].insuredAmount;
+                break;
+            }
+        }
 
-    /**
-     *  @dev Transfers eligible payout funds to insuree
-     *
-     */
-    function pay() external pure {}
+        return payout;
+    }
 
     /**
      * @dev Initial funding for the insurance. Unless there are too many delayed flights
      *      resulting in insurance payouts, the contract should be self-sustaining
      *
      */
-    function fund() public payable {}
+    function fundAirline(address airline)
+        external
+        payable
+        requireIsOperational
+    {
+        Airline memory fundedAirline = operatingAirlines[airline];
+        require(
+            fundedAirline.isRegistered,
+            "Airline can only be funded if it is registered first"
+        );
+
+        operatingAirlines[airline].etherAmount = fundedAirline.etherAmount.add(
+            msg.value
+        );
+        operatingAirlines[airline].isFunded = true;
+        operatingAirlines[airline].isRegistered = true;
+
+        emit FundAirline(airline);
+    }
+
+    /**
+     * @dev Buy insurance for a flight
+     *
+     */
+    function buy(bytes32 flight, address insuree)
+        external
+        payable
+        requireIsOperational
+    {
+        bool isAlreadyInsuree = false;
+        uint256 newInsuredValue = msg.value;
+
+        Insured[] memory insurees = flightInsurees[flight];
+        for (uint256 i = 0; i < insurees.length; i++) {
+            if (insurees[i].addr == insuree) {
+                isAlreadyInsuree = true;
+                //require(contractOwner.balance > amountToPay, "Contract does not have enough payout.");
+                uint256 totalValue = insurees[i].insuredAmount.add(
+                    newInsuredValue
+                );
+
+                if (totalValue > 1) {
+                    uint256 returnOverageValue = totalValue.sub(1);
+                    insurees[i].insuredAmount = totalValue;
+
+                    payable(insuree).transfer(returnOverageValue);
+                }
+                break;
+            }
+        }
+
+        if (!isAlreadyInsuree) {
+            Insured memory newInsuree = Insured(
+                insuree,
+                newInsuredValue,
+                false
+            );
+            flightInsurees[flight].push(newInsuree);
+        }
+
+        emit BuyFlightInsurance(flight, insuree);
+    }
+
+    /**
+     *  @dev Credits payouts to insurees
+     */
+    function creditInsurees(bytes32 flight, address passenger)
+        external
+        requireIsOperational
+    {
+        Insured[] memory insurees = flightInsurees[flight];
+
+        for (uint256 i = 0; i < insurees.length; i++) {
+            if (insurees[i].addr == passenger) {
+                require(!insurees[i].isPayed, "Caller has already been payed.");
+                require(
+                    insurees[i].insuredAmount > 0,
+                    "There is no Insured Amount."
+                );
+                flightInsurees[flight][i].isPayed = true;
+                pay(insurees[i].insuredAmount, passenger);
+                flightInsurees[flight][i].insuredAmount = 0;
+                emit InsurancePayout(flight, passenger);
+                break;
+            }
+        }
+    }
+
+    /**
+     *  @dev Transfers eligible payout funds to insuree
+     *
+     */
+    function pay(uint256 requiredEther, address passenger)
+        public
+        payable
+        requireIsOperational
+    {
+        uint256 amountToPay = requiredEther.add(requiredEther.div(2));
+
+        require(
+            contractOwner.balance > amountToPay,
+            "Contract does not have enough payout."
+        );
+
+        (bool success, ) = passenger.call{value: amountToPay}("");
+        require(success, "Insurance transfer to passenger failed");
+    }
+
+    /**
+     * @dev Initial funding for the insurance. Unless there are too many delayed flights
+     *      resulting in insurance payouts, the contract should be self-sustaining
+     *
+     */
+    function fund(address airline) external payable requireIsOperational {
+        Airline memory fundedAirline = operatingAirlines[airline];
+        require(
+            fundedAirline.isRegistered,
+            "Airline can only be funded if it is registered first"
+        );
+        //require(msg.value >= 12 ether, "Need to fund at least 10 Ether for be be participant");
+        operatingAirlines[airline].etherAmount = fundedAirline.etherAmount.add(
+            msg.value
+        );
+        operatingAirlines[airline].isFunded = true;
+        operatingAirlines[airline].isRegistered = true;
+        /*if(msg.value >= 10 ether){
+            operatingAirlines[airline].isFunded;
+        }*/
+
+        emit FundAirline(airline);
+    }
 
     function getFlightKey(
         address airline,
