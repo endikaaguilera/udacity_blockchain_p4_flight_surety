c0c0f467b58fae8b2744bf28c67ade86453265e7
EndikaAguilera <matiax28@gmail.com>
2022-05-14 22:42:27 +0200

    update testConfig

--- config/testConfig.js
+++ config/testConfig.js
@@ -3,8 +3,8 @@ var FlightSuretyApp = artifacts.require("FlightSuretyApp");
 var FlightSuretyData = artifacts.require("FlightSuretyData");
 var BigNumber = require('bignumber.js');
 
-var Config = async function(accounts) {
-    
+var Config = async function (accounts) {
+
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     let testAddresses = [
@@ -22,14 +22,16 @@ var Config = async function(accounts) {
 
     let owner = accounts[0];
     let firstAirline = accounts[1];
+    let firstPassenger = accounts[3];
 
-    let flightSuretyData = await FlightSuretyData.new();
-    let flightSuretyApp = await FlightSuretyApp.new();
+    let flightSuretyData = await FlightSuretyData.new(firstAirline);
+    let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address);
 
-    
+
     return {
         owner: owner,
         firstAirline: firstAirline,
+        firstPassenger: firstPassenger,
         weiMultiple: (new BigNumber(10)).pow(18),
         testAddresses: testAddresses,
         flightSuretyData: flightSuretyData,
